b4056e00dcbb03b6198c9c38b2f172b7aff1cfcb
giangnv <giangnv@vnext.vn>
2018-10-06 18:25:38 +0700

    aa

--- wp-content/themes/tramdecor/index.php
+++ wp-content/themes/tramdecor/index.php
@@ -1,281 +1,177 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+<!doctype html>
+<html xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"
+      xmlns:website="http://ogp.me/ns/website" lang="en-US" itemscope itemtype="http://schema.org/WebPage">
 
-get_header(); ?>
+<head>
+    <!-- Mirrored from www.tramdecor.com/vn/thiet-ke-noi-that by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 Oct 2018 07:06:22 GMT -->
+    <!-- Added by HTTrack -->
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8"/>
+    <!-- -->
+    <title>
+        <?php the_title() ?>
+    </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <?php wp_head(); ?>
+</head>
 
-<div class="main-wrapper clearfix">
-	<div id="main">
-		<!-- bắt đầu lấy từ đoạn này -->
-		<div class="wth_theme_main">
-			<div class="charm3_head">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-4">
-							<div class="item_h">
-								<h3><a href="" class="font_gothic_bold">Planning for the future</a></h3>
-								<div class="img_h">
-									<a href="" style="background-image:url(img/1.png)"></a>
-								</div>
-								<div class="content_h">
-									<div class="icon_h">
-										<i class="fa fa-line-chart" aria-hidden="true"></i>
-									</div>
-									<div class="text_h font_gothic_r">
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-										Lorem Ipsum is simply dummy
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="item_h">
-								<h3><a href="" class="font_gothic_bold">We’ll Help You Get There</a></h3>
-								<div class="img_h">
-									<a href="" style="background-image:url(img/2.png)"></a>
-								</div>
-								<div class="content_h">
-									<div class="icon_h">
-										<i class="fa fa-laptop" aria-hidden="true"></i>
-									</div>
-									<div class="text_h font_gothic_r">
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-										Lorem Ipsum is simply dummy Lorem Ipsum is simply dummy
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="item_h">
-								<h3><a href="" class="font_gothic_bold">Creating The Life You Love</a></h3>
-								<div class="img_h">
-									<a href="" style="background-image:url(img/6.png)"></a>
-								</div>
-								<div class="content_h">
-									<div class="icon_h">
-										<i class="fa fa-coffee" aria-hidden="true"></i>
-									</div>
-									<div class="text_h font_gothic_r">
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-										Lorem Ipsum is simply dummy text
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="charm3_video">
-				<div class="container">
-					<div class="video_page">
-						<div class="row">
-							<div class="col-md-6">
-								<div class="video_l">
-									<div class="iframe_vd">
-										<div class="embed-responsive embed-responsive-16by9">
-											<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" ></iframe>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-5 col-md-offset-1 col-sm-offset-0 col-xs-offset-0">
-								<div class="video_r">
-									<h3 class="font_gothic_bold">Growing <br> your dream</h3>
-									<div class="content_vd font_gothic_r">
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum,
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="parallax_bg" style="background-image: url('img/bg_f.png');">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-1 col-sm-1">
-							<div class="parallax_icon">
-								<i class="fa fa-envelope" aria-hidden="true"></i>
-							</div>
-						</div>
-						<div class="col-md-7 col-sm-7">
-							<div class="parallax_text">
-								<h5 class="font_gothic_bold">Please feel free to contact us ...</h5>
-								<p class="font_gothic_r">Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-4">
-							<center>
-								<a href="" class="btn_para font_gothic_bold">Contact us</a>
-							</center>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="charm3_service">
-				<div class="container">
-					<h3 class="h3_title_id">Lorem Ipsum</h3>
-					<div class="row">
-						<div class="col-md-7">
-							<div class="service_item">
-								<div class="cel_md_6 cel_md_hv">
-									<div class="i_it">
-										<div class="it_icon"><i class="fa fa-university" aria-hidden="true"></i></div>
-										<div class="it_border"></div>
-										<div class="it_number">01</div>
-									</div>
-									<div class="i_h3">
-										Lorem Ipsum
-									</div>
-									<div class="i_content">
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum
-									</div>
-								</div>
-								<div class="cel_md_6 cel_md_hv">
-									<div class="i_it">
-										<div class="it_icon"><i class="fa fa-graduation-cap" aria-hidden="true"></i></div>
-										<div class="it_border"></div>
-										<div class="it_number">02</div>
-									</div>
-									<div class="i_h3">
-										Lorem Ipsum
-									</div>
-									<div class="i_content">
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum
-									</div>
-								</div>
-								<div class="cel_md_6 cel_md_hv">
-									<div class="i_it">
-										<div class="it_icon"><i class="fa fa-recycle" aria-hidden="true"></i></div>
-										<div class="it_border"></div>
-										<div class="it_number">03</div>
-									</div>
-									<div class="i_h3">
-										Lorem Ipsum
-									</div>
-									<div class="i_content">
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum
-									</div>
-								</div>
-								<div class="cel_md_6 cel_md_hv">
-									<div class="i_it">
-										<div class="it_icon"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></div>
-										<div class="it_border"></div>
-										<div class="it_number">04</div>
-									</div>
-									<div class="i_h3">
-										Lorem Ipsum
-									</div>
-									<div class="i_content">
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-5">
-							<div class="service_img">
-								<img class="img-responsive" src="img/CharmOutsource.png">
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="charm3_testimonials">
-				<div class="container">
-					<h3 class="h3_title_id">See The Testimonials</h3>
-					<div class="row">
-						<div class="col-md-8 col-md-offset-2 col-xs-12">
-							<div class="testimonials_sli">
-								<div class="slider-for">
-									<!-- ảnh lớn -->
-									<div>
-										<div class="img_ts">
-											<img class="img-responsive" src="img/user/1.jpg" alt=""/>
-										</div>
-									</div>
-									<div>
-										<div class="img_ts">
-											<img class="img-responsive" src="img/user/2.jpg" alt=""/>
-										</div>
-									</div>
-									<div>
-										<div class="img_ts">
-											<img class="img-responsive" src="img/user/3.jpg" alt=""/>
-										</div>
-									</div>
-									<div>
-										<div class="img_ts">
-											<img class="img-responsive" src="img/user/4.jpg" alt=""/>
-										</div>
-									</div>
+<body>
+<header id="header" class="row">
+    <div class="container header-inner  align-self-center">
+        <div class="row justify-content-between">
+            <div class="header-left col-4 align-self-center">
+                <a href="/" class="Header-branding">
+                    <img
+                        src="//static1.squarespace.com/static/5b8bf301e2ccd13e972a0ab4/t/5ba8b62b15fcc0772b6ffe75/1538382333870/?format=1500w"
+                        alt="Tramdecor" class="Header-branding-logo">
+                </a>
+            </div>
+            <div class="header-right col-6 align-self-center">
+                <nav class="Header-nav Header-nav--primary" data-nc-element="primary-nav"
+                     data-content-field="navigation">
 
-								</div>
+                    <div class="Header-nav-inner">
+        <span class="Header-nav-item Header-nav-item--folder">
 
-								<div class="slider-nav"> <!-- ảnh nhỏ -->
-									<div>
-										<p class="font_gothic_r">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum.</p>
-										<h5 class="font_gothic_r">Tasnim Akter</h5>
-									</div>
-									<div>
-										<p class="font_gothic_r">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum</p>
-										<h5 class="font_gothic_r">Tasnim Akter</h5>
-									</div>
-									<div>
-										<p class="font_gothic_r">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum</p>
-										<h5 class="font_gothic_r">Tasnim Akter</h5>
-									</div>
-									<div>
-										<p class="font_gothic_r">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy Lorem Ipsum</p>
-										<h5 class="font_gothic_r">Tasnim Akter</h5>
-									</div>
+      <a href="/thiet-ke-noi-that/" class="Header-nav-folder-title" data-controller="HeaderNavFolderTouch"
+         data-controllers-bound="HeaderNavFolderTouch">Thiết kế</a>
 
+      <span class="Header-nav-folder">
 
-								</div>
+              <a href="/vn/thiet-ke-noi-that" class="Header-nav-folder-item">THIẾT KẾ NỘI THẤT</a>
 
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="parallax_bg" style="background-image: url('img/bg_f.png');">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-1 col-sm-1">
-							<div class="parallax_icon">
-								<i class="fa fa-envelope" aria-hidden="true"></i>
-							</div>
-						</div>
-						<div class="col-md-7 col-sm-7">
-							<div class="parallax_text">
-								<h5 class="font_gothic_bold">Please feel free to contact us ...</h5>
-								<p class="font_gothic_r">Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-4">
-							<center>
-								<a href="" class="btn_para font_gothic_bold">Contact us</a>
-							</center>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- kết thúc từ đoạn này -->
-	</div>
+            <a href="/vn/thiet-ke-quan-cafe/" class="Header-nav-folder-item"
+               data-test="template-nav">THIẾT KẾ QUÁN CAFE</a>
+
+            <a href="/vn/thiet-ke-nha-hang/" class="Header-nav-folder-item"
+               data-test="template-nav">THIẾT KẾ NHÀ HÀNG</a>
+
+            <a href="/vn/thiet-ke-cua-hang/" class="Header-nav-folder-item"
+               data-test="template-nav">THIẾT KẾ CỬA HÀNG</a>
+
+            <a href="/vn/thiet-ke-quan-tra-sua/" class="Header-nav-folder-item" data-test="template-nav">THIẾT KẾ QUÁN TRÀ SỮA</a>
+
+            <a href="/vn/thiet-ke-nha/" class="Header-nav-folder-item" data-test="template-nav">THIẾT KẾ NHÀ</a>
+
+            <a href="/vn/thiet-ke-nhan-dien-thuong-hieu/" class="Header-nav-folder-item" data-test="template-nav">THIẾT KẾ LOGO&nbsp;&amp; NHẬN DIỆN THƯƠNG HIỆU</a>
+
+            <a href="/vn/blog/" class="Header-nav-folder-item" data-test="template-nav">BLOG</a>
+
+            <a href="/vn/lien-he/" class="Header-nav-folder-item" data-test="template-nav">Liên hệ</a>
+
+      </span>
+        </span><span class="Header-nav-item Header-nav-item--folder">
+
+      <a href="/design/" class="Header-nav-folder-title" data-controller="HeaderNavFolderTouch"
+         data-controllers-bound="HeaderNavFolderTouch">Design</a>
+
+      <span class="Header-nav-folder">
+
+              <a href="/en/design" class="Header-nav-folder-item">DESIGN</a>
+
+            <a href="/en/home-designs/" class="Header-nav-folder-item" data-test="template-nav">HOME DESIGNS</a>
+
+            <a href="/en/office-designs/" class="Header-nav-folder-item" data-test="template-nav">OFFICE DESIGNS</a>
+
+            <a href="/en/cafe-designs/" class="Header-nav-folder-item" data-test="template-nav">CAFE DESIGNS</a>
+
+            <a href="/en/restaurant-designs/" class="Header-nav-folder-item"
+               data-test="template-nav">RESTAURANT DESIGNS</a>
+
+            <a href="/en/shop-designs/" class="Header-nav-folder-item" data-test="template-nav">SHOP DESIGNS</a>
+
+            <a href="/en/logo-design/" class="Header-nav-folder-item" data-test="template-nav">LOGO DESIGNS</a>
+
+            <a href="/en/brand-design/" class="Header-nav-folder-item" data-test="template-nav">BRAND DESIGNS</a>
+
+      </span>
+        </span><span class="Header-nav-item Header-nav-item--folder">
+
+      <a href="/suunnittelu/" class="Header-nav-folder-title" data-controller="HeaderNavFolderTouch"
+         data-controllers-bound="HeaderNavFolderTouch">Suunnittelu</a>
+
+      <span class="Header-nav-folder">
+
+              <a href="/fi/suunnittelu" class="Header-nav-folder-item">SUUNNITTELU</a>
+
+            <a href="/fi/kodin-sisustus/" class="Header-nav-folder-item" data-test="template-nav">KODIN SISUSTUS</a>
+
+            <a href="/fi/toimiston-sisustus/" class="Header-nav-folder-item"
+               data-test="template-nav">TOIMISTON SISUSTUS</a>
+
+            <a href="/fi/kaupan-sisustus/" class="Header-nav-folder-item" data-test="template-nav">KAUPAN SISUSTUS</a>
+
+            <a href="/fi/kahvilan-sisustus/" class="Header-nav-folder-item"
+               data-test="template-nav">KAHVILAN SISUSTUS</a>
+
+            <a href="/fi/ravintolan-sisustus/" class="Header-nav-folder-item" data-test="template-nav">RAVINTOLAN SISUSTUS</a>
+
+            <a href="/fi/logo-suunnittelu/" class="Header-nav-folder-item" data-test="template-nav">LOGO SUUNNITTELU</a>
+
+            <a href="/fi/graafinen-suunnittelu/" class="Header-nav-folder-item" data-test="template-nav">GRAAFINEN SUUNNITTELU</a>
+
+      </span>
+        </span>
+                    </div>
+
+                </nav>
+            </div>
+        </div>
+
+    </div>
+</header>
+<div class="container">
+    <div class="site-content">
+        <div class="block html-block">
+            <h1>THIẾT KẾ NỘI THẤT CHUYÊN NGHIỆP</h1>
+        </div>
+        <div class="block html-block">
+            <p style="text-align: center; white-space: pre-wrap;">Thiết Kế Trang Trí Nội Thất Và Nhận Diện Thương Hiệu
+                Quán Cafe - Nhà Hàng - Quán Trà Sữa - Karaoke - Cửa Hàng - Nhà Ở - Văn Phòng - Showroom-Spa, Salon.</p>
+            <p style="white-space: pre-wrap;">TRAMDECOR thành lập từ 2013 bởi Fourder Trâm Nguyễn. Chúng tôi là tập hợp những nhà thiết kế sáng tạo cùng định hướng về thẩm mỹ, đam mê lĩnh vực thiết kế và trang trí. Với phong cách làm việc chuyên nghiệp, tâm huyết Tramdecor đã xây dựng được những giá trị nhất định trong dịch vụ của mình. Được khách hàng biết đến như một đơn vị thiết kế trẻ chuyên nghiệp, tận tâm, uy tín về chất lượng thiết kế Ý Tưởng Sáng Tạo - Chỉnh Chu Chi Tiết - Tiến Độ Chuẩn Xác, chúng tôi là đồng hành đắc lực cho nhiều khách hàng cá nhân, doanh nghiệp, start up trong và ngoài nước.</p>
+        </div>
+        <div class="block block-html">
+            <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel" data-interval="2000">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img
+                            src="https://static1.squarespace.com/static/5b8bf301e2ccd13e972a0ab4/5badfae5b208fc9b61a3cec2/5badfae5e5e5f038a94a30ca/1538377730758/cong-ty-thiet-ke-noi-that%2801%29.jpg?format=1500w"
+                            alt="Los Angeles">
+
+                    </div>
+                    <div class="carousel-item">
+                        <img
+                            src="https://static1.squarespace.com/static/5b8bf301e2ccd13e972a0ab4/5badfae5b208fc9b61a3cec2/5badfafe0d929727f6e2b11c/1538377742053/cong-ty-thiet-ke-noi-that%2804%29.jpg?format=1500w"
+                            alt="Chicago">
+
+                    </div>
+                    <div class="carousel-item">
+                        <img
+                            src="https://static1.squarespace.com/static/5b8bf301e2ccd13e972a0ab4/5badfae5b208fc9b61a3cec2/5badfafe8165f51becf53d8f/1538129340854/cong-ty-thiet-ke-noi-that%2803%29.jpg?format=1500w"
+                            alt="New York">
+
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleSlidesOnly" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleSlidesOnly" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+</body>
+
+</html>
+
