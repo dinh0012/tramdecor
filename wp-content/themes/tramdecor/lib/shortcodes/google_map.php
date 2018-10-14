c1481127a86ca75f1fabac43a2e361a5505cf2d4
huydv <huydv@vnext.com.vn>
2018-10-14 21:17:31 +0700

    google_map

---
+++ wp-content/themes/tramdecor/lib/shortcodes/google_map.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dinh0
+ * Date: 10/8/2018
+ * Time: 10:16 PM
+ */
+
+function google_map_shortcode( $atts ) {
+    $width = $atts['width'] ?? '1200';
+    $height = $atts['height'] ?? '1000';
+    $address = $atts['address'] ?? "university%20of%20san%20francisco";
+    $html = '';
+    $html .=  '<div class="mapouter">';
+    $html .=  '<div class="gmap_canvas">';
+    $html .=  '<iframe id="gmap_canvas" src="https://maps.google.com/maps?q=' . htmlentities($address) . '&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" width="' . $width . '" height="' . $height . '" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe><a href="https://www.pureblack.de">pureblack.de</a>';
+    $html .=  '</div>';
+    $html .=  '<style>.mapouter{text-align:right;height:' . $height . 'px;width:' . $width . 'px;}.gmap_canvas {overflow:hidden;background:none!important;height:' . $height . 'px;width:' . $width . 'px;}</style></div>';
+    return $html;
+}
+add_shortcode( 'google_map', 'google_map_shortcode' );
+    
--- wp-content/themes/tramdecor/lib/shortcodes/init_shortcode.php
+++ wp-content/themes/tramdecor/lib/shortcodes/init_shortcode.php
@@ -3,3 +3,4 @@ require_once 'team.php';
 require_once 'btn_yellow.php';
 require_once 'branddesign.php';
 require_once 'text_in_img.php';
+require_once 'google_map.php';
